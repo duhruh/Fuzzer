43d6640463a634a562e27e195148a05d0b2d3d9b
David <David.R.Rivera193@gmail.com>
2012-02-10 10:17:32 -0500

    Added comments and formated the file

--- imagereadintest.c
+++ imagereadintest.c
@@ -1,3 +1,7 @@
+// Program: Fuzzer
+// Authors: David Rivera & Harrison Phillips 
+// Date: 2-10-2012
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,33 +9,41 @@
 
 int main(){
 
-    //unsigned char image[];
-    FILE *ifp;
-    long len=0;
-    ifp=fopen("image.jpg","rb");
+    	FILE *ifp;
+    	long len=0;
+	unsigned char* image;
+    	ifp=fopen("image.jpg","rb");
+    	fseek(ifp,0,SEEK_END);
+    	len = ftell(ifp);
 
-
-    fseek(ifp,0,SEEK_END);
-    len = ftell(ifp);
-unsigned char* image;
-    image =(unsigned char*) malloc(sizeof(char)*len);	
-    fseek(ifp,0,SEEK_SET);
-    fread(image,1,len,ifp);
-    fclose(ifp);
-    printf("the size is %ld\n",len);
+	//Reads in the file and prints out the size
+    	image =(unsigned char*) malloc(sizeof(char)*len);	
+    	fseek(ifp,0,SEEK_SET);
+    	fread(image,1,len,ifp);
+    	fclose(ifp);
+    	printf("the size is %ld\n",len);
 	
+	//Converts the special characters to HEX characters
+	//then prints them out
 	char* buf_str = (char*)malloc(2*len+1);
 	char* buf_ptr = buf_str; 
 	int i = 0;
+	
 	for(i = 0; i < len; i++){
 		buf_ptr += sprintf(buf_ptr,"%02X",image[i]);		
-	//printf("'%s'",image);	}
 	}
-sprintf(buf_ptr,"\n");
-*(buf_ptr + 1) = '\0';
-printf("%s\n", buf_str);
-   
-return 0;
+	
+	sprintf(buf_ptr,"\n");
+	*(buf_ptr + 1) = '\0';
+	printf("%s\n", buf_str);
+	
+	//Loop the number of generations
 
+	//Mutate the file
 
+	//Execute the mutation   
+
+	//Record the mutation
+
+	return 0;
 }
