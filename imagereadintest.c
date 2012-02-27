4753cf4088d9f2a49e832435e704303cb4e4345e
David <David.R.Rivera193@gmail.com>
2012-02-27 15:12:54 -0500

    semi work

--- imagereadintest.c
+++ imagereadintest.c
@@ -6,11 +6,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
 
+void mutate_new(int len, unsigned char* image);
 void mutate(int seed,int len,unsigned char* image);
+int myPow(int x, int p){
+	int result = 1;
+	int inc;
+	for(inc = 0; inc < p; ++inc){
+		result *= x;
+	}
+	printf("made it in the pow function!");
+	return result;
+}
 
-int main(){
-
+int main(int arge,char** argv){
+	if(arge == 0){
+		printf("No arge was inputed to program!:%d",arge);
+	}
     	FILE *ifp;
 	FILE *result;
 	FILE *seeds;
@@ -21,7 +34,7 @@ int main(){
 	char* fileName = "bug.jpg";
 	char fileNum[10];
 	int fileCounter = 10;
-    	ifp=fopen("image.jpg","rb");
+    	ifp=fopen("image.jpg","r");
     	fseek(ifp,0,SEEK_END);
     	len = ftell(ifp);
 	 int q = 0;
@@ -32,13 +45,13 @@ int main(){
     	fread(image,1,len,ifp);
     	fclose(ifp);
 	seeds = fopen("seeds.txt","w+");
-	result = fopen(fileName, "w+");
+	result = fopen(fileName, "w");
     	//printf("the size is %ld\n",len);
-
+	int k =0;
 
 	//Converts the special characters to HEX characters
 	//then prints them out //EDIT not sure what this does
-	char* buf_str = (char*)malloc(2*len+1);
+	/*char* buf_str = (char*)malloc(2*len+1);
 	char* buf_ptr = buf_str; 
 	int i, k = 0;
 	
@@ -47,24 +60,24 @@ int main(){
 	}
 	
 	sprintf(buf_ptr,"\n");
-	*(buf_ptr + 1) = '\0';
+	*(buf_ptr + 1) = '\0';*/
 	/////////////////////////////////////////////////////
-	
-		
+	int seed = atoi(argv[1]);	
+	int loop = atoi(argv[2]);	
 	//Loop the number of generations
-	for(k = 0; k < 1000; k++){
-		rewind(result);
-		bugedImage =(unsigned char*) malloc(sizeof(char)*len);	
-    	
+	for(k = 0; k < loop; k++){
+		result = fopen(fileName, "w");
+		bugedImage =(unsigned char*) malloc(sizeof(unsigned char)*len);	
 		for(q = 0; q < len;q++){
 			bugedImage[q] =  image[q];
-	//		printf("This is bugedImage: %02X  This image: %02X\n",bugedImage[q],image[q]);
+		//	printf("This is bugedImage: %02X  This image: %02X\n",bugedImage[q],image[q]);
 		}	
-		
 		//Mutate the file
-		int seed = abs(time(NULL)*rand());
+		//int seed = atoi(argv[1]);
+		//int seed = abs(rand());
 		srand(seed);
-		mutate(seed,len,bugedImage);
+		mutate_new(len, image);
+		//mutate(seed,len,bugedImage);
 		//image[rand()%len] = (image[rand()%len]+rand()) % 5;
 		//bugedImage[rand()%len] = (image[rand()%len]+rand())%5;
 		//PRINT TO SEEDS IF X ROUNDS HAVE OCCURES
@@ -76,20 +89,23 @@ int main(){
 
 		//Write the file
 		//result = fopen(fileName,"w+");
-		for(i = 0; i < len; i++){
-			fprintf(result,"%c",bugedImage[i]);
-		}
+		fwrite(bugedImage, 1, len, result);
+		/*for(i = 0; i < len; i++){
+			fwrite(bugedImage[i], 1, result);
+		}*/
 		free(bugedImage);
+		fclose(result);
+		//free(bugedImage);
 		//fclose(result);
 		
 		//Execute the mutation
-		int returnVal = system("./jpegconv -ppm bug.jpg > out.txt");
+		int returnVal = system("./jpegconv -ppm bug.jpg 1> out.txt");
 		//fprintf(stderr, "%d",returnVal);
 		//long bug = system("./jpegconv -ppm bug.jpg");
 		//Record the mutation
 		//printf("This is the returnVal: %d\n",returnVal);
 		if(returnVal != 0){
-			//printf("This is the bugged returnVal: %d\n",returnVal);
+		//	printf("This is the bugged returnVal: %d\n",returnVal);
 			//seeds = fopen("seeds.txt","w+");
 			fprintf(seeds,"%d\n",seed);
 			sprintf(fileNum,"%d\n",fileCounter);
@@ -97,25 +113,53 @@ int main(){
 			//fileName = strcat("bug",".jpg");
 			//fclose(seeds);
 			fileCounter++;
+			seed = rand();
+		}else{
+			seed = rand();
 		}
 	}
-	fclose(result);
+	free(image);
 	fclose(seeds);
 	return 0;
 }
 
+void mutate_new(int len, unsigned char* image){
+	int i = 0;
+	int rand = rand();
+	for(i = 0; i < len; ++i){
+		if((rand()%100) < 25 && i > 4){
+			image[i] = ((int)pow(-1, rand()))*rand()%256;
+			image[i] %= 256;
+		}else if((rand()%100) > 25 && (rand()%100)< 50 && i > 4){
+			image[i] = (image[i] >> rand()) %256;
+		}else if((rand()%100) > 50 && (rand()%100)< 75 && i > 4){
+			//image[i] += rand()%256;
+		}else if((rand()%100) > 75 && (rand()%100)<=100 && i > 4){
+			//image[i] = (image[i] << rand())%256;
+		}
+	}
+}
 
 void mutate(int seed,int len, unsigned char* image){
-	int u = 0;
+	int u = 5+rand()%len;
+	int loop = 5+rand()%len;
 	seed++;
 	seed--;
 	len++;
 	len--;
 	//image[10] = 100;
-	for(u=len/2; u < len/2+50; u++){
-		//image[u] = image[u] >> 1;
-		//printf("This is image: %02X\n", image[u]+1);
-		image[u] = (image[u]+(seed%256));
-		//image[(abs(seed%len-20))+u]=image[u]%5;
+	if((loop+5)%len != 0){
+		for(u = loop; u < loop+5; u++){
+			//image[u] = image[u] >> 1;
+			//printf("This is image: %02X\n", image[u]+1);
+			//image[u] = (image[u]+(seed%256));
+			image[u] = 100;
+			//image[(abs(seed%len-20))+u]=image[u]%5;
+		}
+	}else if((loop+5)%len == 0){
+		loop = loop-10;
+		for(u = loop; u < loop+5; u++){
+			image[u] = 100;
+		}
 	}
 }
